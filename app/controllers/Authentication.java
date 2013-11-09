d67b65f1d32912a564000f1a9ca6cbd4e66722f0
Kirthi Banothu <banothu2@illinois.edu>
2013-11-09 12:55:07 -0600

    FUCK

--- app/controllers/Authentication.java
+++ app/controllers/Authentication.java
@@ -6,10 +6,33 @@ import play.mvc.*;
 import views.html.*;
 
 public class Authentication extends Controller {
-
-    public static Result register() {
+	
+    public static Result registerPageRender() {
         return ok(register.render("Please Register at Frat-Base"));
     }
+
+    public static Result registerUser(){
+    	return ok(register.render("hello"));
+    }
+
+	public class User {
+		@Required
+	    public String email;
+	    public String password;
+
+	    public String validate() {
+	        if(authenticate(email,password) == null) {
+	            return "Invalid email or password";
+	        }
+	        return null;
+	    }
+	}
+
+	Form<User> userForm = form(User.class);
+
+
+
+
 
 
 }
