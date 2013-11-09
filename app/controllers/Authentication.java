c71850316c9e77b49d933d4b5cf6ffb693b37e7f
Kirthi Banothu <banothu2@illinois.edu>
2013-11-09 12:55:21 -0600

    Revert 066bddb..d67b65f
    This rolls back to commit 066bddb4e3e95c363f336d66e6bdd792937a9b76.

--- app/controllers/Authentication.java
+++ app/controllers/Authentication.java
@@ -6,33 +6,10 @@ import play.mvc.*;
 import views.html.*;
 
 public class Authentication extends Controller {
-	
-    public static Result registerPageRender() {
+
+    public static Result register() {
         return ok(register.render("Please Register at Frat-Base"));
     }
-
-    public static Result registerUser(){
-    	return ok(register.render("hello"));
-    }
-
-	public class User {
-		@Required
-	    public String email;
-	    public String password;
-
-	    public String validate() {
-	        if(authenticate(email,password) == null) {
-	            return "Invalid email or password";
-	        }
-	        return null;
-	    }
-	}
-
-	Form<User> userForm = form(User.class);
-
-
-
-
 
 
 }
