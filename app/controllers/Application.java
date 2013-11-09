d6357efe7431d17c8dab7f9b7851d23e81e16b5f
Kirthi Banothu <banothu2@illinois.edu>
2013-11-09 12:01:42 -0600

    Commit All

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,10 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render("Your new application is Ready NOt!."));
     }
 
+    public static Result loggedIn(){
+    	return ok(loggedIn.render("You are logged in!"));
+    }
 }
